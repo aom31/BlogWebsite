409a14e6aff6057541fb7c7f1d96493bc77cff88
Thamakorn Ketnoi <thamakorn.k@siriustech.io>
2022-10-17 14:04:26 +0700

    login

---
+++ authorization/result.md
@@ -0,0 +1,3 @@
+login after register
+
+image.png
--- authorization/route/router.go
+++ authorization/route/router.go
@@ -2,12 +2,11 @@ package route
 
 import (
 	"forPractice/projectGolang/BlogWebsite/authorization/controller"
-	"forPractice/projectGolang/BlogWebsite/authorization/middleware"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func Setup(app *fiber.App) {
-	app.Use(middleware.IsAuthenicate)
+	//app.Use(middleware.IsAuthenicate)
 	app.Post("/api/blogweb/login", controller.Login)
 }
